c5146138e4311bb91bccc61530d4e5c71e890edf
rust <wozdabady@MacBook-Air-wozdabady.local>
2021-08-03 22:25:08 +0300

    Initial commit

---
+++ celsiusToFahrenheitConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  celsiusToFahrenheitConverter
+//
+//  Created by wozdabady on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //dfdv
+    }
+
+
+}
+
